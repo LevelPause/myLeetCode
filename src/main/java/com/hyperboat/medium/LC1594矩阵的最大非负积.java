e485e8c117aacc4c0ed4618d44d5708cf8f35511
hyperboat <brothernone@126.com>
2023-10-18 08:18:42 +0800

    LC1594矩阵的最大非负积

--- src/main/java/com/hyperboat/medium/LC1594矩阵的最大非负积.java
+++ src/main/java/com/hyperboat/medium/LC1594矩阵的最大非负积.java
@@ -1,5 +1,7 @@
 package com.hyperboat.medium;
 
+import java.util.Arrays;
+
 /**
  * @author zhangweigang
  * @date 2023年10月18日 5:47
@@ -7,22 +9,37 @@ package com.hyperboat.medium;
 public class LC1594矩阵的最大非负积 {
 
   public int maxProductPath(int[][] grid) {
-    int i = 0, j = 0;
-    int res = dp(i, j, grid, 1);
-    return res > 0 ? res % ((int) Math.pow(10, 9) + 7) : -1;
+    int mod = 1000000007;
+    int m = grid.length, n = grid[0].length;
+    //这里一定要写long而不是int, int会在乘积的过程中溢出导致结果不正确
+    long[][] dpMax = new long[m][n];
+    long[][] dpMin = new long[m][n];
+    dpMax[0][0] = dpMin[0][0] = grid[0][0];
+    if (grid[0][0] == 0) {
+      return 0;
+    }
+    //当i==0时,只能往右走, 所以得到的积为第一行的积
+    for (int j = 1; j < n; j++) {
+      dpMax[0][j] = dpMin[0][j] = grid[0][j] * dpMax[0][j - 1];
+    }
+
+    //当j==0时,只能往下走, 所以得到的积为第一列的积
+    for (int i = 1; i < m; i++) {
+      dpMax[i][0] = dpMin[i][0] = grid[i][0] * dpMax[i - 1][0];
+    }
+
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        if (grid[i][j] < 0) {
+          dpMax[i][j] = Math.min(dpMin[i - 1][j], dpMin[i][j - 1]) * grid[i][j];
+          dpMin[i][j] = Math.max(dpMax[i - 1][j], dpMax[i][j - 1]) * grid[i][j];
+        } else {
+          dpMax[i][j] = Math.max(dpMax[i - 1][j], dpMax[i][j - 1]) * grid[i][j];
+          dpMin[i][j] = Math.min(dpMin[i - 1][j], dpMin[i][j - 1]) * grid[i][j];
+        }
+      }
+    }
+    return (int) Math.max(dpMax[m - 1][n - 1] % mod, -1);
   }
 
-  private int dp(int i, int j, int[][] grid, int res) {
-    if (i == grid.length - 1 && j == grid[0].length - 1) {
-      return res * grid[i][j];
-    }
-    if (i == grid.length - 1) {
-      return res * dp(i, j + 1, grid, res);
-    }
-    if (j == grid[0].length - 1) {
-      return res * dp(i + 1, j, grid, res);
-    }
-    return Math
-        .max(Math.abs(res * dp(i, j + 1, grid, res)), Math.abs(res * dp(i + 1, j, grid, res)));
-  }
 }
