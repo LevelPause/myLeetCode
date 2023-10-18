a314f4bf1ae03d7181b3711d62d38434677488cd
hyperboat <brothernone@126.com>
2023-10-19 02:07:40 +0800

    LC189轮转数组

---
+++ src/test/java/com/hyperboat/面试经典150题/LC189轮转数组Test.java
@@ -0,0 +1,23 @@
+package com.hyperboat.面试经典150题;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author zhangweigang
+ * @date 2023年10月18日 12:47
+ */
+class LC189轮转数组Test {
+
+  @Test
+  void rotate() {
+    int[] ints = {2, 1};
+    new LC189轮转数组().rotate(ints, 1);
+    assertArrayEquals(new int[]{1, 2}, ints);
+    int[] ints1 = {1, 2, 3, 4, 5, 6, 7};
+    new LC189轮转数组().rotate1(ints1, 3);
+    assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, ints1);
+  }
+}
