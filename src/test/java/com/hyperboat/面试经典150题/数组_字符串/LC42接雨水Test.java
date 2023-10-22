931c2249969ad61fd4ed2d2cd594b04667ed1250
hyperboat <brothernone@126.com>
2023-10-22 08:39:30 +0800

    LC42接雨水

---
+++ src/test/java/com/hyperboat/面试经典150题/数组_字符串/LC42接雨水Test.java
@@ -0,0 +1,18 @@
+package com.hyperboat.面试经典150题.数组_字符串;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author zhangweigang
+ * @date 2023年10月21日
+ */
+class LC42接雨水Test {
+
+  @Test
+  void trap() {
+//    assertEquals(6, new LC42接雨水().trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    assertEquals(9, new LC42接雨水().trap(new int[]{4,2,0,3,2,5}));
+  }
+}
