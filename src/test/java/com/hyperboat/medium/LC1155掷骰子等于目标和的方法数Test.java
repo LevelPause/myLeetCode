73c620c71a177c5553fde527aac8ac38c99c936e
hyperboat <brothernone@126.com>
2023-10-24 17:53:48 +0800

    LC1155掷骰子等于目标和的方法数(每日一题)

---
+++ src/test/java/com/hyperboat/medium/LC1155掷骰子等于目标和的方法数Test.java
@@ -0,0 +1,19 @@
+package com.hyperboat.medium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author zhangweigang
+ * @date 2023年10月24日
+ */
+class LC1155掷骰子等于目标和的方法数Test {
+
+  @Test
+  void numRollsToTarget() {
+    assertEquals(1, new LC1155掷骰子等于目标和的方法数().numRollsToTarget(1, 6, 3));
+    assertEquals(6, new LC1155掷骰子等于目标和的方法数().numRollsToTarget(2, 6, 7));
+    assertEquals(222616187, new LC1155掷骰子等于目标和的方法数().numRollsToTarget(30, 30, 500));
+  }
+}
