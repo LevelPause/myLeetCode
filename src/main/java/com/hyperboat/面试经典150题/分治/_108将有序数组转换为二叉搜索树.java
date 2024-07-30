98074f036dc812fe9e480c66194026dec4927a16
hyperboat <weigangzhang2@gmail.com>
2024-07-30 18:49:02 +0800

    2024.07.30

---
+++ src/main/java/com/hyperboat/面试经典150题/分治/_108将有序数组转换为二叉搜索树.java
@@ -0,0 +1,48 @@
+package com.hyperboat.面试经典150题.分治;
+
+/**
+ * https://leetcode.cn/problems/convert-sorted-array-to-binary-search-tree/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class _108将有序数组转换为二叉搜索树 {
+
+  int[] nums;
+
+  public TreeNode sortedArrayToBST(int[] nums) {
+    int n = nums.length;
+    this.nums = nums;
+    return buildTree(0, n);
+  }
+
+  public TreeNode buildTree(int start, int end) {
+    if (start > end) {
+      return null;
+    }
+    int mid = (start + end) / 2;
+    TreeNode curr = new TreeNode(nums[(start + end) / 2]);
+    curr.left = buildTree(start, mid - 1);
+    curr.right = buildTree(start, mid + 1);
+    return curr;
+  }
+
+  class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
+//  0 1 2 3 4 5 6 7 8
+
---
+++ src/main/java/com/hyperboat/面试经典150题/回溯/_22括号生成.java
@@ -0,0 +1,38 @@
+package com.hyperboat.面试经典150题.回溯;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _22括号生成 {
+
+  List<String> ans = new ArrayList<>();
+  int n;
+  char[] line;
+
+  public List<String> generateParenthesis(int n) {
+    this.n = n;
+    line = new char[n * 2];
+    backTrace(n, n, 0);
+    return ans;
+  }
+
+  public void backTrace(int l, int r, int index) {
+    if (l > r || index == n * 2) {
+      return;
+    }
+    if (l == 0) {
+      Arrays.fill(line, index, n * 2, ')');
+      System.out.println(line);
+      ans.add(new String(line));
+      return;
+    }
+
+//    选左括号
+    line[index] = '(';
+    backTrace(l - 1, r, index + 1);
+    //选右括号
+    line[index] = ')';
+    backTrace(l, r - 1, index + 1);
+  }
+}
