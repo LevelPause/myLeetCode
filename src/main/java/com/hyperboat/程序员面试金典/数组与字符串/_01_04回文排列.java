7676163b9d1e88cba6b24d7c410782f6a04dee02
hyperboat <weigangzhang2@gmail.com>
2024-08-05 23:45:55 +0800

    20240805

---
+++ src/main/java/com/hyperboat/程序员面试金典/数组与字符串/_01_04回文排列.java
@@ -0,0 +1,23 @@
+package com.hyperboat.程序员面试金典.数组与字符串;
+
+/*
+ * https://leetcode.cn/problems/palindrome-permutation-lcci/?envType=study-plan-v2&envId=cracking-the-coding-interview
+ * */
+public class _01_04回文排列 {
+
+  public boolean canPermutePalindrome(String s) {
+    int[] b = new int[128];
+    for (char c : s.toCharArray()) {
+      if (b[c] != 0) {
+        b[c]--;
+      } else {
+        b[c]++;
+      }
+    }
+    int ans = 0;
+    for (int i : b) {
+      ans += i;
+    }
+    return ans <= 1;
+  }
+}
---
+++ src/test/java/com/hyperboat/daily/y2024/m08/d05/_600不含连续1的非负整数Test.java
@@ -0,0 +1,13 @@
+package com.hyperboat.daily.y2024.m08.d05;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class _600不含连续1的非负整数Test {
+
+  @Test
+  void findIntegers() {
+    assertEquals(5,new _600不含连续1的非负整数().findIntegers(5));
+  }
+}
