d2f608f33dab8d9ece0b82d8ba299c25c239093c
hyperboat <weigangzhang2@gmail.com>
2024-08-13 13:51:21 +0800

    202408013-daily

--- src/main/java/com/hyperboat/daily/y2024/m08/d13/_3151_特殊数组I.java
+++ src/main/java/com/hyperboat/daily/y2024/m08/d13/_3151_特殊数组I.java
@@ -1,19 +1,25 @@
 package com.hyperboat.daily.y2024.m08.d13;
 
+/*
+ * https://leetcode.cn/problems/special-array-i/
+ * */
 public class _3151_特殊数组I {
 
-  public boolean isArraySpecial(int[] nums) {
-    int n = nums.length;
-    if (n < 2) {
+  static class Solution {
+
+    public boolean isArraySpecial(int[] nums) {
+      int n = nums.length;
+      if (n < 2) {
+        return true;
+      }
+
+      for (int i = 1; i < n; i++) {
+        if ((nums[i] & 1) == (nums[i - 1] & 1)) {
+          return false;
+        }
+      }
+
       return true;
     }
-
-    for (int i = 1; i < n; i++) {
-      if ((nums[i] & 1) == (nums[i - 1] & 1)) {
-        return false;
-      }
-    }
-
-    return true;
   }
 }
