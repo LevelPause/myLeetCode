634f90e39aa9fa39597054cbc1d740fc458c5e89
hyperboat <weigangzhang2@gmail.com>
2024-08-18 13:40:13 +0800

    202408018～daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m08/d18/_551_学生出勤记录I.java
@@ -0,0 +1,33 @@
+package com.hyperboat.daily.y2024.m08.d18;
+
+public class _551_学生出勤记录I {
+
+  static class Solution {
+
+    public boolean checkRecord(String s) {
+      int cntC = 0;
+      int index = 0;
+      int n = s.length();
+      while (index < n) {
+        char c = s.charAt(index);
+        if (c == 'A') {
+          if (++cntC == 2) {
+            return false;
+          }
+          index++;
+        } else if (c == 'L') {
+          int cnt = 1;
+          while (++index < n && s.charAt(index) == 'L') {
+            if (++cnt >= 3) {
+              return false;
+            }
+          }
+        } else {
+          index++;
+        }
+      }
+      return true;
+    }
+  }
+
+}
