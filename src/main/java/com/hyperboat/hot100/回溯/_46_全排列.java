5ac06415a280a5a477a8bd249554ceaa1d8ebda8
hyperboat <weigangzhang2@gmail.com>
2024-08-22 00:38:20 +0800

    202408021

---
+++ src/main/java/com/hyperboat/hot100/回溯/_46_全排列.java
@@ -0,0 +1,33 @@
+package com.hyperboat.hot100.回溯;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _46_全排列 {
+
+  class Solution {
+
+    public List<List<Integer>> permute(int[] nums) {
+      List<List<Integer>> res = new ArrayList<>();
+      backTrace(new boolean[nums.length], nums, res, new ArrayList<>());
+      return res;
+    }
+
+    public void backTrace(boolean[] visited, int[] nums, List<List<Integer>> res,
+        List<Integer> line) {
+      if (line.size() == nums.length) {
+        res.add(new ArrayList<>(line));
+        return;
+      }
+      for (int i = 0; i < nums.length; i++) {
+        if (!visited[i]) {
+          visited[i] = true;
+          line.add(nums[i]);
+          backTrace(visited, nums, res, line);
+          line.remove(line.size() - 1);
+          visited[i] = false;
+        }
+      }
+    }
+  }
+}
---
+++ src/test/java/com/hyperboat/hot100/图论/_207_课程表Test.java
@@ -0,0 +1,12 @@
+package com.hyperboat.hot100.图论;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class _207_课程表Test {
+  @Test
+  public void test(){
+    assertTrue(new _207_课程表.Solution().canFinish(2,new int[0][0]));
+  }
+}
