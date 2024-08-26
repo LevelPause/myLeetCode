198cf7b11daeb41df98e87a5910e883c88033a1e
hyperboat <weigangzhang2@gmail.com>
2024-08-27 01:00:28 +0800

    202408226

---
+++ src/main/java/com/hyperboat/hot100/二分/_33_搜索旋转排序数组.java
@@ -0,0 +1,69 @@
+package com.hyperboat.hot100.二分;
+
+public class _33_搜索旋转排序数组 {
+
+  class Solution {
+
+    public int search(int[] nums, int target) {
+      return binarySearchOnce(nums, target);
+    }
+
+    public int binarySearchOnce(int[] nums, int target) {
+      int n = nums.length;
+      int l = 0;
+      int r = n - 1;
+      while (l <= r) {
+        int m = (l + r) >> 1;
+        if (nums[m] == target) {
+          return m;
+        }
+        if (nums[0] <= nums[m]) {
+          if (nums[0] <= target && target < nums[m]) {
+            r = m - 1;
+          } else {
+            l = m + 1;
+          }
+        } else {
+          if (nums[m] < target && target <= nums[n - 1]) {
+            l = m + 1;
+          } else {
+            r = m - 1;
+          }
+        }
+
+      }
+      return -1;
+    }
+
+    public int binarySearchTwice(int[] nums, int target) {
+      int n = nums.length;
+      int l = 0;
+      int r = n - 1;
+      int min = 0;
+      while (l <= r) {
+        int m = (l + r) >> 1;
+        if (nums[0] < nums[m]) {
+          l = m + 1;
+        } else {
+          r = m - 1;
+          min = m;
+        }
+      }
+      l = min;
+      r = l + n - 1;
+      while (l <= r) {
+        int m = (l + r) >> 1;
+        int index = m % n;
+        if (nums[index] == target) {
+          return index;
+        }
+        if (target < nums[index]) {
+          r = m - 1;
+        } else {
+          l = m + 1;
+        }
+      }
+      return -1;
+    }
+  }
+}
---
+++ src/main/java/com/hyperboat/hot100/二分/_4_寻找两个正序数组的中位数.java
@@ -0,0 +1,36 @@
+package com.hyperboat.hot100.二分;
+
+public class _4_寻找两个正序数组的中位数 {
+
+  class Solution {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+      int m = nums1.length;
+      int n = nums2.length;
+      //保证m<=n
+      if (m > n) {
+        return findMedianSortedArrays(nums2, nums1);
+      }
+      int l = 0;
+      int r = m;
+      int half = (m + n + 1) >> 1;
+      int resL = 0, resR = 0;
+      while (l <= r) {
+        int i = (l + r) >> 1;
+        int j = half - i;
+        int numI = i == m ? Integer.MAX_VALUE : nums1[i];
+        int numI_1 = i == 0 ? Integer.MIN_VALUE : nums1[i - 1];
+        int numJ = j == n ? Integer.MAX_VALUE : nums2[j];
+        int numJ_1 = j == 0 ? Integer.MIN_VALUE : nums2[j - 1];
+        if (numI_1 <= numJ) {
+          resL = Math.max(numI_1, numJ_1);
+          resR = Math.min(numI, numJ);
+          l = i + 1;
+        } else {
+          r = i - 1;
+        }
+      }
+      return ((m + n) & 1) == 1 ? resL : (resL + resR) / 2.0d;
+    }
+  }
+}
