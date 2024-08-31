4727ebd3bb13d8f3a44e26bc456ab48b607dbb95
hyperboat <weigangzhang2@gmail.com>
2024-08-31 22:16:42 +0800

    20240831

---
+++ src/main/java/com/hyperboat/hot100/多维动态规划/_72_编辑距离.java
@@ -0,0 +1,32 @@
+package com.hyperboat.hot100.多维动态规划;
+
+public class _72_编辑距离 {
+
+  class Solution {
+
+    public int minDistance(String word1, String word2) {
+      int m = word1.length();
+      int n = word2.length();
+      if (m == 0 || n == 0) {
+        return m + n;
+      }
+      int[][] dp = new int[m + 1][n + 1];
+      for (int i = 0; i <= m; i++) {
+        dp[i][0] = i;
+      }
+      for (int i = 0; i <= n; i++) {
+        dp[0][i] = i;
+      }
+      for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+          int up = dp[i - 1][j] + 1;
+          int left = dp[i][j - 1] + 1;
+          int upLeft = word1.charAt(i) == word2.charAt(j) ? dp[i - 1][j - 1] : dp[i - 1][j - 1] + 1;
+          dp[i][j] = Math.min(Math.min(up, left), upLeft);
+        }
+      }
+
+      return dp[m][n];
+    }
+  }
+}
---
+++ src/test/java/com/hyperboat/hot100/动态规划/_416_分割等和子集Test.java
@@ -0,0 +1,12 @@
+package com.hyperboat.hot100.动态规划;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class _416_分割等和子集Test {
+@Test
+  public void test(){
+  assertTrue(new _416_分割等和子集.Solution().canPartition(new int[]{1,5,11,5}));
+}
+}
