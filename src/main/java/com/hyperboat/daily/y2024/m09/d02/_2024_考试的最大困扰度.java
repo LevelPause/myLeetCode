827942c0a6573d1d47e7f72203bc0da25de01a81
hyperboat <weigangzhang2@gmail.com>
2024-09-02 13:10:49 +0800

    20240902～daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m09/d02/_2024_考试的最大困扰度.java
@@ -0,0 +1,32 @@
+package com.hyperboat.daily.y2024.m09.d02;
+
+public class _2024_考试的最大困扰度 {
+
+  class Solution {
+
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+      return Math.max(getCnt('T', answerKey, k), getCnt('F', answerKey, k));
+    }
+
+    private int getCnt(char c, String s, int k) {
+      int n = s.length();
+      int l = 0, r = 0;
+      int cntC = 0;
+      int len = 0;
+      while (r < n) {
+        if (s.charAt(r) == c) {
+          cntC++;
+        }
+        while (l < r && cntC > k) {
+          if (s.charAt(l++) == c) {
+            cntC--;
+            break;
+          }
+        }
+        len = Math.max(len, r - l + 1);
+        r++;
+      }
+      return len;
+    }
+  }
+}
