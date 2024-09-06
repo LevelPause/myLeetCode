3a0ed7063c939f1c768a022716e4f8cbdeaf3207
hyperboat <weigangzhang2@gmail.com>
2024-09-06 10:45:12 +0800

    20240905~daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m09/d05/_3174_清除数字.java
@@ -0,0 +1,20 @@
+package com.hyperboat.daily.y2024.m09.d05;
+
+public class _3174_清除数字 {
+
+  class Solution {
+
+    public String clearDigits(String s) {
+      char[] chars = s.toCharArray();
+      StringBuilder sb = new StringBuilder();
+      for (char c : chars) {
+        if (Character.isDigit(c) && sb.length() > 0) {
+          sb.deleteCharAt(sb.length() - 1);
+        } else {
+          sb.append(c);
+        }
+      }
+      return sb.toString();
+    }
+  }
+}
