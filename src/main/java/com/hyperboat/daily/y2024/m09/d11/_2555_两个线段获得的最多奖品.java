c16041d7b8d4ac837768c533c13509518e17dfd2
hyperboat <weigangzhang2@gmail.com>
2024-09-14 16:28:25 +0800

    20240910~14～daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m09/d11/_2555_两个线段获得的最多奖品.java
@@ -0,0 +1,28 @@
+package com.hyperboat.daily.y2024.m09.d11;
+
+/*
+ * https://leetcode.cn/problems/maximize-win-from-two-segments/description/
+ * */
+public class _2555_两个线段获得的最多奖品 {
+
+  class Solution {
+
+    public int maximizeWin(int[] prizePositions, int k) {
+      int n = prizePositions.length;
+      if (k * 2 + 1 >= prizePositions[n - 1] - prizePositions[0]) {
+        return n;
+      }
+      int ans = 0;
+      int left = 0;
+      int[] mx = new int[n + 1];
+      for (int right = 0; right < n; right++) {
+        while (prizePositions[right] - prizePositions[left] > k) {
+          left++;
+        }
+        ans = Math.max(ans, mx[left] + right - left + 1);
+        mx[right + 1] = Math.max(mx[right], right - left + 1);
+      }
+      return ans;
+    }
+  }
+}
---
+++ src/main/java/com/hyperboat/daily/y2024/m09/d12/_2576_求出最多标记下标.java
@@ -0,0 +1,24 @@
+package com.hyperboat.daily.y2024.m09.d12;
+
+import java.util.Arrays;
+
+/*
+ * https://leetcode.cn/problems/find-the-maximum-number-of-marked-indices/description/
+ * */
+public class _2576_求出最多标记下标 {
+
+  class Solution {
+
+    public int maxNumOfMarkedIndices(int[] nums) {
+      Arrays.sort(nums);
+      int l = 0;
+      int n = nums.length;
+      for (int r = (n + 1) / 2; r < n; r++) {
+        if (nums[l] * 2 <= nums[r]) {
+          l++;
+        }
+      }
+      return l * 2;
+    }
+  }
+}
