d89425f4d0ec7da931a6635d094038a05d9aa867
hyperboat <weigangzhang2@gmail.com>
2024-09-22 15:03:33 +0800

    20240922～daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m09/d22/_997_找到小镇的法官.java
@@ -0,0 +1,27 @@
+package com.hyperboat.daily.y2024.m09.d22;
+
+/*
+ * https://leetcode.cn/problems/find-the-town-judge/submissions/566856174/
+ * */
+public class _997_找到小镇的法官 {
+
+  class Solution {
+
+    public int findJudge(int n, int[][] trust) {
+      int[] degree = new int[n + 1];
+      int max = 1;
+      for (int[] row : trust) {
+        degree[row[0]] -= 1;
+        degree[row[1]] += 1;
+        if (degree[row[1]] > degree[max]) {
+          max = row[1];
+        }
+      }
+      if (degree[max] == n - 1) {
+        return max;
+      } else {
+        return -1;
+      }
+    }
+  }
+}
