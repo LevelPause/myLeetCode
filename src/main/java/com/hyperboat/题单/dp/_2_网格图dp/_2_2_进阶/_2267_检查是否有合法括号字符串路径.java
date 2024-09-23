5f5d86cb90e8aeac741c4ad597bac695fd5bd548
hyperboat <weigangzhang2@gmail.com>
2024-09-23 11:02:07 +0800

    20240922～网格图dp专项练习(进阶)

---
+++ src/main/java/com/hyperboat/题单/dp/_2_网格图dp/_2_2_进阶/_2267_检查是否有合法括号字符串路径.java
@@ -0,0 +1,44 @@
+package com.hyperboat.题单.dp._2_网格图dp._2_2_进阶;
+
+/*
+ * https://leetcode.cn/problems/check-if-there-is-a-valid-parentheses-string-path/
+ * */
+public class _2267_检查是否有合法括号字符串路径 {
+
+  class Solution {
+
+    private int m;
+    private int n;
+    private boolean[][][] visited;
+    private char[][] grid;
+
+    public boolean hasValidPath(char[][] grid) {
+      m = grid.length;
+      n = grid[0].length;
+      this.grid = grid;
+      //路径长度一定要是偶数, 路径长度为(m+n-1)
+      if ((m + n) % 2 == 0 || grid[0][0] == ')' || grid[m - 1][n - 1] == '(') {
+        return false;
+      }
+      visited = new boolean[m][n][(m + n + 1) / 2];
+      return dfs(0, 0, 0);
+    }
+
+    private boolean dfs(int i, int j, int cnt) {
+      //左括号过多, 剩余路径全是右括号也不够匹配
+      if (cnt > m - i + n - j - 1) {
+        return false;
+      }
+      if (i == m - 1 && j == n - 1) {
+        return cnt == 1;
+      }
+      //重复访问
+      if (visited[i][j][cnt]) {
+        return false;
+      }
+      visited[i][j][cnt] = true;
+      cnt += grid[i][j] == '(' ? 1 : -1;
+      return cnt >= 0 && (i < m - 1 && dfs(i + 1, j, cnt) || j < n - 1 && dfs(i, j + 1, cnt));
+    }
+  }
+}
