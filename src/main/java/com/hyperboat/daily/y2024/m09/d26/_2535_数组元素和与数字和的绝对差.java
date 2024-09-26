dcd7e76169768b02fdd25dac54865a086335dc6a
hyperboat <weigangzhang2@gmail.com>
2024-09-26 10:30:32 +0800

    20240926～daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m09/d26/_2535_数组元素和与数字和的绝对差.java
@@ -0,0 +1,23 @@
+package com.hyperboat.daily.y2024.m09.d26;
+
+/*
+ * https://leetcode.cn/problems/difference-between-element-sum-and-digit-sum-of-an-array/description/
+ * */
+public class _2535_数组元素和与数字和的绝对差 {
+
+  class Solution {
+
+    public int differenceOfSum(int[] nums) {
+      int sum1 = 0;
+      int sum2 = 0;
+      for (int num : nums) {
+        sum1 += num;
+        while (num > 0) {
+          sum2 += num % 10;
+          num /= 10;
+        }
+      }
+      return Math.abs(sum1 - sum2);
+    }
+  }
+}
