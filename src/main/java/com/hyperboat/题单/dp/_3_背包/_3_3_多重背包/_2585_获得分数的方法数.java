bbad521c5ce2ff67088e92605371b8bd6de481c8
hyperboat <weigangzhang2@gmail.com>
2024-09-27 11:47:49 +0800

    test push

---
+++ src/main/java/com/hyperboat/题单/dp/_3_背包/_3_3_多重背包/_2585_获得分数的方法数.java
@@ -0,0 +1,15 @@
+package com.hyperboat.题单.dp._3_背包._3_3_多重背包;
+
+/*
+ * https://leetcode.cn/problems/number-of-ways-to-earn-points/description/
+ * */
+public class _2585_获得分数的方法数 {
+
+  // todo
+  class Solution {
+
+    public int waysToReachTarget(int target, int[][] types) {
+      return 0;
+    }
+  }
+}
