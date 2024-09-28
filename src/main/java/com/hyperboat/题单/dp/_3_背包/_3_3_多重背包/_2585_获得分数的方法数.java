9861945b1ea78a3bc6cc413d81b16d4e600a9ab7
hyperboat <weigangzhang2@gmail.com>
2024-09-29 01:19:06 +0800

    20240928~dp专项训练(多重背包)

--- src/main/java/com/hyperboat/题单/dp/_3_背包/_3_3_多重背包/_2585_获得分数的方法数.java
+++ src/main/java/com/hyperboat/题单/dp/_3_背包/_3_3_多重背包/_2585_获得分数的方法数.java
@@ -5,11 +5,24 @@ package com.hyperboat.题单.dp._3_背包._3_3_多重背包;
  * */
 public class _2585_获得分数的方法数 {
 
-  // todo
   class Solution {
 
+    private static final int MOD = (int) 1e9 + 7;
+
     public int waysToReachTarget(int target, int[][] types) {
-      return 0;
+      int n = types.length;
+      int[] dp = new int[target + 1];
+      dp[0] = 1;
+      for (int[] type : types) {
+        int count = type[0];
+        int mark = type[1];
+        for (int j = target; j > 0; j--) {
+          for (int k = 1; k <= count && k <= j / mark; k++) {
+            dp[j] = (dp[j] + dp[j - k * mark]) % MOD;
+          }
+        }
+      }
+      return dp[target];
     }
   }
 }
