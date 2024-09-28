d9912b4816b64093313d8e97293375d504125f27
hyperboat <weigangzhang2@gmail.com>
2024-09-29 01:18:21 +0800

    20240929~daily(秒杀简单题)

---
+++ src/main/java/com/hyperboat/daily/y2024/m09/d29/_2073_买票需要的时间.java
@@ -0,0 +1,24 @@
+package com.hyperboat.daily.y2024.m09.d29;
+
+/*
+ *
+ * https://leetcode.cn/problems/time-needed-to-buy-tickets/description/?envType=daily-question&envId=2024-09-29
+ * */
+public class _2073_买票需要的时间 {
+
+  class Solution {
+
+    public int timeRequiredToBuy(int[] tickets, int k) {
+      int ans = 0;
+      int max = tickets[k];
+      for (int i = 0; i < tickets.length; i++) {
+        if (i <= k) {
+          ans += Math.min(max, tickets[i]);
+        } else {
+          ans += Math.min(max - 1, tickets[i]);
+        }
+      }
+      return ans;
+    }
+  }
+}
