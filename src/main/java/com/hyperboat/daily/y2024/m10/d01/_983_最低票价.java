441d93bdb70abe5b96936bb9492d35f4cefd2bfa
hyperboat <weigangzhang2@gmail.com>
2024-10-01 01:38:43 +0800

    20241001~daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m10/d01/_983_最低票价.java
@@ -0,0 +1,25 @@
+package com.hyperboat.daily.y2024.m10.d01;
+
+import java.util.Arrays;
+
+public class _983_最低票价 {
+
+  class Solution {
+
+    public int mincostTickets(int[] days, int[] costs) {
+      int n = days.length;
+      int max = days[n - 1];
+      int[] dp = new int[max + 1];
+      for (int i = 1, j = 0; j < n && i <= max; i++) {
+        dp[i] = dp[i - 1];
+        if (i == days[j]) {
+          dp[i] = dp[Math.max(0, i - 1)] + costs[0];
+          dp[i] = Math.min(dp[i], dp[Math.max(0, i - 7)] + costs[1]);
+          dp[i] = Math.min(dp[i], dp[Math.max(0, i - 30)] + costs[2]);
+          j++;
+        }
+      }
+      return dp[max];
+    }
+  }
+}
