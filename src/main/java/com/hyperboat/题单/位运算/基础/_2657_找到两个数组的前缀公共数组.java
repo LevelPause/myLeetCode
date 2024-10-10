f531ecaa0f4d234cf8366ba926d6940821c682e5
hyperboat <weigangzhang2@gmail.com>
2024-10-11 01:19:16 +0800

    20241010~位运算专项练习~基础

---
+++ src/main/java/com/hyperboat/题单/位运算/基础/_2657_找到两个数组的前缀公共数组.java
@@ -0,0 +1,23 @@
+package com.hyperboat.题单.位运算.基础;
+
+/*
+ * https://leetcode.cn/problems/find-the-prefix-common-array-of-two-arrays/description/
+ * */
+public class _2657_找到两个数组的前缀公共数组 {
+
+  class Solution {
+
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+      int n = A.length;
+      long maskA = 0L;
+      long maskB = 0L;
+      int[] ans = new int[n];
+      for (int i = 0; i < n; i++) {
+        maskA |= 1L << A[i];
+        maskB |= 1L << B[i];
+        ans[i] = Long.bitCount(maskA & maskB);
+      }
+      return ans;
+    }
+  }
+}
