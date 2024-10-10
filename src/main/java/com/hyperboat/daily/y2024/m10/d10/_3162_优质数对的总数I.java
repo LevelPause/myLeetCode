76bf456a36014299a59e241e21b1e2df6f13c233
hyperboat <weigangzhang2@gmail.com>
2024-10-10 11:02:12 +0800

    20241010~daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m10/d10/_3162_优质数对的总数I.java
@@ -0,0 +1,40 @@
+package com.hyperboat.daily.y2024.m10.d10;
+
+import java.util.HashMap;
+import java.util.Map;
+/*
+* https://leetcode.cn/problems/find-the-number-of-good-pairs-i/description/
+* */
+public class _3162_优质数对的总数I {
+
+  class Solution {
+
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+      int max = 0;
+      for (int x : nums1) {
+        max = Math.max(x, max);
+      }
+      int[] dp = new int[max / k + 1];
+      for (int x : nums1) {
+        if (x % k == 0) {
+          dp[x / k]++;
+        }
+      }
+      int ans = 0;
+      Map<Integer, Integer> cnt = new HashMap<>();
+      for (int y : nums2) {
+        if (cnt.containsKey(y)) {
+          ans += cnt.get(y);
+        } else {
+          int occ = 0;
+          for (int i = y; i <= max / k; i += y) {
+            occ += dp[i];
+          }
+          ans += occ;
+          cnt.put(y, occ);
+        }
+      }
+      return ans;
+    }
+  }
+}
