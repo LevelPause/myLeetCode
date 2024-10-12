b2d48cf1353b0113dab52796c1bfc6ffda8a14a4
hyperboat <weigangzhang2@gmail.com>
2024-10-12 21:03:23 +0800

    20241011~位运算专项训练~拆位法贡献法

---
+++ src/main/java/com/hyperboat/题单/位运算/拆位法贡献法/_2275_按位与结果大于零的最长组合.java
@@ -0,0 +1,22 @@
+package com.hyperboat.题单.位运算.拆位法贡献法;
+
+/*
+ * https://leetcode.cn/problems/largest-combination-with-bitwise-and-greater-than-zero/description/
+ * */
+public class _2275_按位与结果大于零的最长组合 {
+
+  class Solution {
+
+    public int largestCombination(int[] candidates) {
+      int ans = 0;
+      for (int i = 0; i < 24; i++) {
+        int cnt = 0;
+        for (int x : candidates) {
+          cnt += x >> i & 1;
+        }
+        ans = Math.max(ans, cnt);
+      }
+      return ans;
+    }
+  }
+}
