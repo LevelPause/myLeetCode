fbfdc10b2563a715522ce5cec7694c9e4ab11c1f
hyperboat <weigangzhang2@gmail.com>
2024-10-15 23:37:34 +0800

    20221015~位运算专项训练~思维题

---
+++ src/main/java/com/hyperboat/题单/位运算/思维题/_810_黑板异或游戏.java
@@ -0,0 +1,18 @@
+package com.hyperboat.题单.位运算.思维题;
+
+/**
+ * https://leetcode.cn/problems/chalkboard-xor-game/description/
+ */
+public class _810_黑板异或游戏 {
+
+  class Solution {
+
+    public boolean xorGame(int[] nums) {
+      var xor = 0;
+      for (int x : nums) {
+        xor ^= x;
+      }
+      return xor == 0 || (nums.length & 1) == 0;
+    }
+  }
+}
