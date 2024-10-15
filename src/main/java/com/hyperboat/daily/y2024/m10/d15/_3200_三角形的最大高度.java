2d2034e32fb1dc8ff3960944318eee18c282c059
hyperboat <weigangzhang2@gmail.com>
2024-10-15 09:55:30 +0800

    20241015~daily

---
+++ src/main/java/com/hyperboat/daily/y2024/m10/d15/_3200_三角形的最大高度.java
@@ -0,0 +1,36 @@
+package com.hyperboat.daily.y2024.m10.d15;
+
+/**
+ * https://leetcode.cn/problems/maximum-height-of-a-triangle/description/
+ */
+public class _3200_三角形的最大高度 {
+
+  class Solution {
+
+    public int maxHeightOfTriangle(int red, int blue) {
+      return O1(red, blue);
+    }
+
+    private int On(int red, int blue) {
+      var cnt = new int[2];
+      int i = 0;
+      while (true) {
+        cnt[i & 1] += i;
+        if ((cnt[0] > red || cnt[1] > blue) && (cnt[1] > red || cnt[0] > blue)) {
+          return i - 1;
+        }
+        i++;
+      }
+    }
+
+    private int O1(int red, int blue) {
+      return Math.max(solve(red, blue), solve(blue, red));
+    }
+
+    private int solve(int m, int n) {
+      var odd = (int) Math.sqrt(n);
+      var even = (int) ((Math.sqrt(m * 4 + 1) - 1) / 2);
+      return odd > even ? even * 2 + 1 : odd * 2;
+    }
+  }
+}
